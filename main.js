0a7a25c7e23e217959c328c337cd7d4ed7731404
mahmoudessam16 <mahmoudemourad29@gmail.com>
2022-11-27 14:25:06 +0200

    Process Of Adding files is done

---
+++ main.js
@@ -0,0 +1,9 @@
+let subscribeEmail = document.getElementById("Form");
+subscribeEmail.onsubmit = function (e) {
+    e.preventDefault();
+}
+// Contact Form
+let contactForm =  document.getElementById("contact");
+contactForm.onsubmit = function (e) {
+    e.preventDefault();
+}
